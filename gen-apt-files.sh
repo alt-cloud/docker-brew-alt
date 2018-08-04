f18bc1ea9cd7dcb3213e5df6ae321dd01e90eb1c
Mikhail Gordeev <obirvalger@altlinux.org>
2018-08-05 01:04:46 +0300

    Add generation of apt.conf and sources.list files

---
+++ gen-apt-files.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -efu
+
+# Generate apt.conf and sources.list files for some (listed below) arches and
+# repositories. Save it in the directory `apt' by default, but it can be
+# changed using firt command line argument.
+
+ROOT="file:///space/ALT" # Place with ALT repositories
+#ROOT="http://ftp.altlinux.org/pub/distributions/ALTLinux"
+ARCHES="i586 x86_64 aarch64"
+REPOS="p8 Sisyphus"
+APT_DIR="${1:-apt}"
+
+mkdir -p "$APT_DIR"
+APT_DIR="$(realpath "$APT_DIR")"
+pushd "$APT_DIR" &> /dev/null
+
+for ARCH in $ARCHES
+do
+    for REPO in $REPOS
+    do
+        REPO_NORM="$(echo "$REPO" | tr '[:upper:]' '[:lower:]')"
+        cat > "$APT_DIR/apt.conf.${REPO_NORM}.${ARCH}" <<EOF
+Dir::Etc::main "/dev/null";
+Dir::Etc::parts "/var/empty";
+Dir::Etc::SourceList "$APT_DIR/sources.list.${REPO_NORM}.${ARCH}";
+Dir::Etc::SourceParts "/var/empty";
+Dir::Etc::preferences "/dev/null";
+Dir::Etc::preferencesparts "/var/empty";
+EOF
+        cat >  "$APT_DIR/sources.list.${REPO_NORM}.${ARCH}" <<EOF
+rpm $ROOT/$REPO $ARCH classic
+rpm $ROOT/$REPO noarch classic
+EOF
+    done
+done
+
+popd &> /dev/null
--- prep-docker-brew-branch.sh
+++ prep-docker-brew-branch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -exu
 #
 # Prep a docker-brew-alt branch
 #
@@ -9,6 +9,17 @@ PROGNAME=$(basename "$0")
 
 DATE=$(date +%Y%m%d)
 
+
+temp_dir=
+f_cleanup_temp_dir()
+{
+    [ -z "$temp_dir" ] || rm -rf -- "$temp_dir"
+    exit "$@"
+}
+
+temp_dir=$(mktemp -dt "${0##*/}.XXXXXXXX")
+trap 'f_cleanup_temp_dir $?' EXIT
+trap 'exit 143' HUP QUIT PIPE TERM
 
 f_ctrl_c() {
     printf "\n*** Exiting ***\n"
@@ -53,34 +64,35 @@ then
     exit 1
 fi
 
-# Build the image
-temp_dir=$(mktemp -d)
-workspace_dir="${temp_dir}/workspace"
-pushd ${temp_dir} &> /dev/null
+# Generate apt.conf and sources.list files
+./gen-apt-files.sh "${temp_dir}/apt"
 
+# Build the image
+workspace_dir="${temp_dir}/workspace"
+mkdir -p "$workspace_dir"
+pushd "$workspace_dir"
+out_dir="${temp_dir}/out"
+mkdir -p "$out_dir"
     # git clone mkimage-profile
     git clone git://git.altlinux.org/people/mike/packages/mkimage-profiles.git
 
     # build image
     cd mkimage-profiles
 
-    mkdir -p "${workspace_dir}/out"
-
     for arch in i586 x86_64 aarch64
     do
         make \
-        APTCONF="${PROGDIR}/apt/apt.conf.${ALT_RELEASE}.${arch}" \
+        APTCONF="${temp_dir}/apt/apt.conf.${ALT_RELEASE}.${arch}" \
         CLEAN=1 \
         QUIET=1 \
         NICE=1 \
-        IMAGEDIR="${workspace_dir}/out" \
+        IMAGEDIR="${workspace_dir}" \
         ARCH="${arch}" \
-        REPORT=1 \
-        ve/docker.tar.xz
+        "ve/docker-${ALT_RELEASE}.tar.xz"
 
-        mkdir -p "${workspace_dir}/${arch}"
-        mv "${workspace_dir}/out/docker-${DATE}-${arch}.tar.xz" "${workspace_dir}/${arch}/alt-${ALT_RELEASE}-${arch}-${DATE}.tar.xz"
-        cat > ${workspace_dir}/${arch}/Dockerfile <<EOF
+        mkdir -p "${out_dir}/${arch}"
+        mv "${workspace_dir}/docker-${ALT_RELEASE}-${DATE}-${arch}.tar.xz" "${out_dir}/${arch}/alt-${ALT_RELEASE}-${arch}-${DATE}.tar.xz"
+        cat > ${out_dir}/${arch}/Dockerfile <<EOF
 FROM scratch
 
 MAINTAINER \\
@@ -94,6 +106,17 @@ CMD ["/bin/bash"]
 EOF
 
     done
-rm -rf "${workspace_dir}/out"
 
 popd &> /dev/null
+
+# Create empty branch to avoid storing of many tarballs in git
+branch_name="$ALT_RELEASE"
+git branch -D "$branch_name" &>/dev/null ||:
+export GIT_WORK_TREE="$out_dir"
+git checkout --orphan "$branch_name"
+git rm -r .
+
+git add .
+git commit -m "Update altlinux $ALT_RELEASE - $DATE"
+
+git checkout master
